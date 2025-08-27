df59aed6099cfe65f4a240963e12bd2f03e54037
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2025-08-27 13:51:27 +0700

    Fix component Suspense in Product

--- src/app/san-pham/page.js
+++ src/app/san-pham/page.js
@@ -1,14 +1,18 @@
+import { Suspense } from 'react';
 import { getMetadata } from '../../utils/helper-server';
 import ProductList from './_components/product-list';
 
 export const metadata = getMetadata({
   title: 'Sản Phẩm | Diệp Trà',
-  description:
-    'Khám phá bộ sưu tập nguyên liệu pha chế cao cấp từ Diệp Trà - Siro, mứt, bột kem và nhiều sản phẩm chất lượng khác.'
+  description: 'Khám phá bộ sưu tập nguyên liệu pha chế cao cấp từ Diệp Trà...'
 });
 
 const ProductPage = () => {
-  return <ProductList />;
+  return (
+    <Suspense fallback={<div>Đang tải sản phẩm...</div>}>
+      <ProductList />
+    </Suspense>
+  );
 };
 
 export default ProductPage;
