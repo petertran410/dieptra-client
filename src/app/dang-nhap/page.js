bed0846b00886392e677bd8ea666c93b2bdf4c84
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2025-10-02 11:59:15 +0700

    Fix related products

--- src/app/dang-nhap/page.js
+++ src/app/dang-nhap/page.js
@@ -1,10 +1,15 @@
+import { Suspense } from 'react';
 import { getMetadata } from '../../utils/helper-server';
 import LoginWrapper from './_components/login-wrapper';
 
 export const metadata = getMetadata({ title: 'Đăng nhập tài khoản' });
 
 const Login = () => {
-  return <LoginWrapper />;
+  return (
+    <Suspense fallback={<div style={{ minHeight: '100vh' }} />}>
+      <LoginWrapper />
+    </Suspense>
+  );
 };
 
 export default Login;
--- src/app/san-pham/huong-dan/page.js
+++ src/app/san-pham/huong-dan/page.js
@@ -1,10 +1,15 @@
+import { Suspense } from 'react';
 import { getMetadata } from '../../../utils/helper-server';
 import ProductGuideWrapper from './_components/product-guide-wrapper';
 
 export const metadata = getMetadata({ title: 'Hướng dẫn' });
 
 const ProductGuide = () => {
-  return <ProductGuideWrapper />;
+  return (
+    <Suspense fallback={<div style={{ minHeight: '100vh' }} />}>
+      <ProductGuideWrapper />
+    </Suspense>
+  );
 };
 
 export default ProductGuide;
