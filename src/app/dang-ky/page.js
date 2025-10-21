011b73080aae4a68f8ae740698ff5137bfd38c4e
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2025-10-21 11:56:24 +0700

    Add privacy facebook

--- src/app/dang-ky/page.js
+++ src/app/dang-ky/page.js
@@ -1,10 +1,15 @@
+import { Suspense } from 'react';
 import { getMetadata } from '../../utils/helper-server';
 import RegisterWrapper from './_components/register-wrapper';
 
 export const metadata = getMetadata({ title: 'Đăng ký tài khoản' });
 
 const Register = () => {
-  return <RegisterWrapper />;
+  return (
+    <Suspense fallback={<div style={{ minHeight: '100vh' }} />}>
+      <RegisterWrapper />
+    </Suspense>
+  );
 };
 
 export default Register;
