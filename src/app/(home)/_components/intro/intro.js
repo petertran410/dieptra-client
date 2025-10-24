839045f1cbcc5107dbfc7b70cfb8cc5602ffaceb
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2025-10-24 18:02:52 +0700

    change homepage UI

--- src/app/(home)/_components/intro/intro.js
+++ src/app/(home)/_components/intro/intro.js
@@ -6,12 +6,12 @@ const HomeIntro = () => {
   return (
     <Flex
       // px={{ xs: '15px', md: '30px', lg: '160px', xl: '200px', '2xl': '250px' }}
-      gap={{ xs: '36px', lg: '80px' }}
-      align="center"
-      pt={{ xs: '200px', md: '450px', lg: '360px', xl: '750px' }}
-      pb={{ xs: '40px', lg: '100px' }}
-      pos="relative"
-      justify="space-between"
+      // gap={{ xs: '36px', lg: '80px' }}
+      // align="center"
+      // pt={{ xs: '200px', md: '450px', lg: '360px', xl: '650px' }}
+      // pb={{ xs: '40px', lg: '100px' }}
+      // pos="relative"
+      // justify="space-between"
       bgGradient="linear(to-b, #a3dcf3 0%, #FFF 50%, #FFF 100%)"
     >
       <Image
@@ -19,11 +19,12 @@ const HomeIntro = () => {
         alt={IMG_ALT}
         w="full"
         h="auto"
+        pt={{ xs: '70px', md: '0' }}
         objectFit="contain"
         objectPosition="bottom"
-        pos="absolute"
-        bottom={0}
-        left={0}
+        // pos="absolute"
+        // bottom={0}
+        // left={0}
       />
     </Flex>
   );
