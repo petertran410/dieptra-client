8e42d31c6a369dbf8b81dba8eb18f24b33495089
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2025-12-06 13:07:51 +0700

    redirect to page

--- src/app/dang-nhap/_components/login-wrapper.js
+++ src/app/dang-nhap/_components/login-wrapper.js
@@ -108,6 +108,9 @@ const LoginWrapper = () => {
 
   const handleGoogleLogin = () => {
     const state = encodeURIComponent(redirectTo);
+    console.log('🔍 redirectTo:', redirectTo);
+    console.log('🔍 state:', state);
+    console.log('🔍 Full URL:', `${API_URL}/api/client-auth/google?state=${state}`);
     window.location.href = `${API_URL}/api/client-auth/google?state=${state}`;
   };
 
